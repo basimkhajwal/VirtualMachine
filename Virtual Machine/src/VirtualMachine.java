a162c7a9fed6040bd8e1426b2d210af6d44b2353
Basim Khajwal <basimkhajwal@gmail.com>
2014-12-13 17:25:06 +0000

    Added entry point and window

---
+++ Virtual Machine/src/VirtualMachine.java
@@ -0,0 +1,9 @@
+
+public class VirtualMachine {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Virtual Machine/src/Window.java
@@ -0,0 +1,95 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window implements Runnable {
+
+	final int WIDTH = 800;
+	final int HEIGHT = 600;
+
+	JFrame frame;
+	Canvas canvas;
+	BufferStrategy bufferStrategy;
+
+	public Window() {
+		frame = new JFrame("Virtual Machine");
+
+		JPanel panel = (JPanel) frame.getContentPane();
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		panel.setLayout(null);
+
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, WIDTH, HEIGHT);
+		canvas.setIgnoreRepaint(true);
+
+		panel.add(canvas);	
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
+
+		canvas.createBufferStrategy(2);
+		bufferStrategy = canvas.getBufferStrategy();
+
+		canvas.requestFocus();
+	}
+
+	long desiredFPS = 60;
+	long desiredDeltaLoop = (1000 * 1000 * 1000) / desiredFPS;
+
+	boolean running = true;
+
+	public void run() {
+
+		long beginLoopTime;
+		long endLoopTime;
+		long currentUpdateTime = System.nanoTime();
+		long lastUpdateTime;
+		long deltaLoop;
+
+		while (running) {
+			beginLoopTime = System.nanoTime();
+
+			render();
+
+			lastUpdateTime = currentUpdateTime;
+			currentUpdateTime = System.nanoTime();
+			update((int) ((currentUpdateTime - lastUpdateTime) / (1000 * 1000)));
+
+			endLoopTime = System.nanoTime();
+			deltaLoop = endLoopTime - beginLoopTime;
+
+			if (deltaLoop > desiredDeltaLoop) {
+				
+			} else {
+				try {
+					Thread.sleep((desiredDeltaLoop - deltaLoop) / (1000 * 1000));
+				} catch (InterruptedException e) {
+					
+				}
+			}
+		}
+	}
+
+	private void render() {
+		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		render(g);
+		g.dispose();
+		bufferStrategy.show();
+	}
+	
+	protected void update(int deltaTime) {
+		
+	}
+
+	protected void render(Graphics2D g) {
+		
+	}
+
+}
