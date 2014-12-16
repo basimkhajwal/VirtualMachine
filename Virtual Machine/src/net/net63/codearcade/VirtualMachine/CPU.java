f12c07dd2c22e18f9bdbced32338472f875071fd
Basim Khajwal <basimkhajwal@gmail.com>
2014-12-16 19:43:59 +0000

    Finished adding instruction set

--- Virtual Machine/src/net/net63/codearcade/VirtualMachine/CPU.java
+++ Virtual Machine/src/net/net63/codearcade/VirtualMachine/CPU.java
@@ -6,8 +6,38 @@ public class CPU implements ICPU{
 	
 	//Registers
 	private int pcRegister;
-	private int addressRegister;
-	private int dataRegsiter;
+	private short addressRegister;
+	private short dataRegister;
+	
+	//Flags
+	private boolean flagZero;
+	private boolean flagLessThan;
+	
+	//BIT Masks
+	private class MASKS{
+		public static final int INT_TO_USHORT = 0x0000FFFF;
+	}
+	
+	private class BITS{
+		//Instruction bits
+		public static final int INSTRUCTION_TYPE = 0;
+		public static final int A = 3;
+		public static final int C1 = 4;
+		public static final int C2 = 5;
+		public static final int C3 = 6;
+		public static final int C4 = 7;
+		public static final int C5 = 8;
+		public static final int C6 = 9;
+		public static final int D1 = 10;
+		public static final int D2 = 11;
+		public static final int D3 = 12;
+		public static final int J1 = 13;
+		public static final int J2 = 14;
+		public static final int J3 = 15;
+		
+		//General purpose bits
+		public static final int SHORT_SIGN_BIT = 15;
+	}
 	
 	private int currentInstruction;
 	
@@ -25,22 +55,118 @@ public class CPU implements ICPU{
 	}
 	
 	private void execute(){
-		if(getBit(currentInstruction, 0) == 0){
-			log("Executing Compute Intruction");
+		if(getBit(currentInstruction, BITS.INSTRUCTION_TYPE)){
+			log("Executing Address Intruction");
 			
+			//Set the address
+			addressRegister = (short) currentInstruction;
 			
+			//Remove the first bit
+			addressRegister = (short) (addressRegister >> 1);
+			
+			log("Changed Address To: "  + addressRegister + ", " + String.format("%15sh", Integer.toUnsignedString(addressRegister, 2) ) .replace(' ', '0')  );
 			
 		}else{
-			log("Executing Address Intruction");
+			log("Executing Compute Intruction");
+			
+			//Integers for unsigned arithmetic and holding values
+			int a;
+			int b;
+			int out;
+			
+			//If A bit is set then use memory at address register otherwise use register itself
+			if(getBit(currentInstruction, BITS.A)){
+				a = RAM.getWord(addressRegister);
+			}else{
+				a = addressRegister;
+			}
+			
+			//Set b to the dataRegister
+			b = dataRegister;
+			
+			//If bit is set then set A to 0
+			if(getBit(currentInstruction, BITS.C1)){
+				a = 0;
+			}
+			
+			//If bit is set then set A to not A, mask to make sure that unsigned shorts don't become negative
+			if(getBit(currentInstruction, BITS.C2)){
+				a = (~a) & MASKS.INT_TO_USHORT;
+			}
+			
+			//If bit is set then set B to 0
+			if(getBit(currentInstruction, BITS.C3)){
+				a = 0;
+			}
+			
+			//If bit is set then set B to not B and mask to make sure that unsigned shorts don't become negative
+			if(getBit(currentInstruction, BITS.C4)){
+				a = (~a) & MASKS.INT_TO_USHORT;
+			}
+			
+			//If function is set then ADD A and B otherwise AND them
+			if(getBit(currentInstruction, BITS.C5)){
+				out = a + b;
+			}else{
+				out = a & b;
+			}
+			
+			//If negate bit is there then negate output
+			if(getBit(currentInstruction, BITS.C6)){
+				out = (~out) & MASKS.INT_TO_USHORT;
+			}
+			
+			//Set all the flags
+			flagZero = out == 0;
+			flagLessThan = getBit(out, BITS.SHORT_SIGN_BIT);
+			
+			//Store the outputs
+			log("OUTPUT: " + out);
+			
+			//If A bit is set then store it in the A-register
+			if(getBit(currentInstruction, BITS.D1)){
+				addressRegister = (short) out;
+			}
+			
+			//If D bit is set then store it in the D-register
+			if(getBit(currentInstruction, BITS.D2)){
+				dataRegister = (short) out;
+			}
+			
+			//If A[M] bit is set then store it in the A[M]-register
+			if(getBit(currentInstruction, BITS.D3)){
+				RAM.setWord(addressRegister, (short) out);
+			}
+			
+			boolean jump = false;
+			
+			//Jump flags
+			if(getBit(currentInstruction, BITS.J1) && flagLessThan){
+				jump = true;
+			}
+			
+			if(getBit(currentInstruction, BITS.J2) && flagZero){
+				jump = true;
+			}
+			
+			if(getBit(currentInstruction, BITS.J3) && !flagLessThan){
+				jump = true;
+			}
+			
+			//JUMP if jump is true
+			if(jump){
+				pcRegister = addressRegister;
+			}
 		}
 	}
 	
-	private int getBit(int value, int position){
-		return (value >> position) & 0x01;
+	private boolean getBit(int value, int position){
+		return ((value >> position) & 0x01) == 1;
 	}
 	
 	private void fetch(){
 		currentInstruction = RAM.getWord(pcRegister);
+		log(String.format("%15sh", Integer.toUnsignedString(currentInstruction, 2) ) .replace(' ', '0'));
 		incPC();
 	}
 
