ad5e4ec0bd1f0d39aceec748214fe9ad3ec052f9
Basim Khajwal <basimkhajwal@gmail.com>
2014-12-29 15:50:19 +0000

    Added more gui

--- Virtual Machine/src/net/net63/codearcade/VirtualMachine/Window.java
+++ Virtual Machine/src/net/net63/codearcade/VirtualMachine/Window.java
@@ -2,19 +2,26 @@ package net.net63.codearcade.VirtualMachine;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -27,29 +34,69 @@ public class Window implements Runnable, KeyListener{
 	private Canvas canvas;
 	private BufferStrategy bufferStrategy;
 	
+	private boolean machineSetup;
 	private boolean machineRunning;
 	
 	private Machine machine;
 	
 	private void newMachineFromFile(File file){
 		
+		try {
+			//Get the file reader
+			InputStream reader = new FileInputStream(file);
+			
+			//Read the data into an array
+			int[] code = new int[Constants.SEGMENTS.CODE.getLength()];
+			for(int i = 0; i < code.length; i++){
+				int next = reader.read();
+				if(next < 0) break;
+				code[i] = next;
+			}
+			
+			//Close the reader
+			reader.close();
+			
+			//Create a new machine
+			machine = new Machine();
+			
+			//Put the code into memory
+			machine.loadCode(code);
+			
+			//The machine is now loaded
+			machineSetup = true;
+			
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(frame, "Error loading this file, try again");
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(frame, "Error reading fromt the file specified, try again");
+		} 
+		
 	}
 	
 	private void setupGUI(JPanel panel){
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		panel.setLayout(new BorderLayout());
+		
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, 400, 400);
+		canvas.setIgnoreRepaint(true);
+		
 		JTabbedPane tabbedPane = new JTabbedPane();
 		
 		JPanel videoMemory = new JPanel();
 		videoMemory.setSize(canvas.getWidth(), canvas.getHeight());
 		videoMemory.add(canvas);
-		tabbedPane.add("Video Memory", videoMemory);
-		
-		
+		tabbedPane.add("Memory", videoMemory);
 		
 		panel.add(tabbedPane, BorderLayout.LINE_START);
 		
-		
 		JPanel controls = new JPanel();
-		controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
+		
+		GridLayout grid = new GridLayout(4, 1);
+		grid.setHgap(100);
+		
+		controls.setLayout(grid);
+		controls.setSize(800 - tabbedPane.getWidth(), controls.getHeight());
 		controls.setFocusable(false);
 		
 		JButton loadProgram = new JButton("Load Program");
@@ -69,30 +116,46 @@ public class Window implements Runnable, KeyListener{
 		
 		JButton runProgram = new JButton("Run Program");
 		runProgram.setFocusable(false);
+		runProgram.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(machineSetup){
+					machineRunning = true;
+				}else{
+					JOptionPane.showMessageDialog(frame, "Please load a machine first!");
+				}
+			}
+		});
+		
+		JButton pauseProgram = new JButton("Pause Program");
+		pauseProgram.setFocusable(false);
+		
+		JButton stopProgram = new JButton("Stop Program");
+		stopProgram.setFocusable(false);
 		
 		controls.add(loadProgram);
 		controls.add(runProgram);
+		controls.add(pauseProgram);
+		controls.add(stopProgram);
 		
-		panel.add(controls, BorderLayout.LINE_END);
+		panel.add(controls, BorderLayout.CENTER);
 	}
 	
 	public Window() {
 		frame = new JFrame("Virtual Machine");
 		
+		machineRunning = false;
+		machineSetup = false;
+		
 		JPanel panel = (JPanel) frame.getContentPane();
-		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		panel.setLayout(new BorderLayout());
-		
-		canvas = new Canvas();
-		canvas.setBounds(0, 0, 400, 400);
-		canvas.setIgnoreRepaint(true);
-		
 		setupGUI(panel); 	
-
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setVisible(true);
+		
 		
 		panel.addKeyListener(this);
 		panel.setFocusable(true);
@@ -103,8 +166,6 @@ public class Window implements Runnable, KeyListener{
 		bufferStrategy = canvas.getBufferStrategy();
 		
 		canvas.setFocusable(false);
-		
-		machine = new Machine();
 	}
 
 	private long desiredFPS = 60;
@@ -157,13 +218,19 @@ public class Window implements Runnable, KeyListener{
 	
 	
 	protected void update(int deltaTime) {
-		machine.update(deltaTime);
+		if(machineRunning){
+			machine.update(deltaTime);
+		}
 	}
 
 	protected void render(Graphics2D g) {
-		if(machine.isUpdated()){
-			g.clearRect(0, 0, WIDTH, HEIGHT);
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		
+		if(machineRunning && machine.isUpdated()){
 			g.drawImage(machine.getVideoBuffer(), 0, 0, 400, 400, 0, 0, 100, 100, null);
+		}else if(!machineRunning){
+			g.setColor(Color.RED);
+			g.drawString("Machine Not Loaded", 50, 50);
 		}
 	}
 
