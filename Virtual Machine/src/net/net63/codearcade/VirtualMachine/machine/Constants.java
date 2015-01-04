febc7c7e409f2e89b9c47bc33c231941d7e2f66b
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-04 17:19:31 +0000

    Changed video memory from 100x100 to 50x50 to make more room for data

--- Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/Constants.java
+++ Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/Constants.java
@@ -6,14 +6,14 @@ public class Constants {
 	
 	public static final int MEMORY_SIZE = (int) Math.pow(2, WORD_SIZE - 1);
 	
-	public static final int VIDEO_WIDTH = 100;
-	public static final int VIDEO_HEIGHT = 100;
+	public static final int VIDEO_WIDTH = 50;
+	public static final int VIDEO_HEIGHT = 50;
 	
 	public static final int[] COLOR_VALUES = new int[]{0,64,128,255};
 	
 	public static class SEGMENTS{
 		public static final Segment CODE = new Segment(0, 10000);
-		public static final Segment VIDEO = new Segment(CODE.getEndPoint(), 10000);
+		public static final Segment VIDEO = new Segment(CODE.getEndPoint(), 2500);
 		public static final Segment KEYBOARD = new Segment(VIDEO.getEndPoint(), 300);
 		public static final Segment DATA = new Segment(KEYBOARD.getEndPoint(), MEMORY_SIZE - VIDEO.getEndPoint() );
 	
