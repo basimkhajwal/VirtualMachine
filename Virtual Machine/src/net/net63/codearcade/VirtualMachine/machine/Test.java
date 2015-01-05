0a48805af3e83c9903a2eef6b1d4665669aae522
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-05 17:31:03 +0000

    Made redundant now, will be removed later

--- Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/Test.java
+++ Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/Test.java
@@ -3,11 +3,11 @@ package net.net63.codearcade.VirtualMachine.machine;
 public class Test {
 	
 	public static void main(String[] args){
-		Memory mem = new Memory(10);
+		//Memory mem = new Memory(10);
 		
-		mem.setWord(0, 1 << 10);
+		//mem.setWord(0, 1 << 10);
 		
-		System.out.println(mem.getWord(0));
+		//S/ystem.out.println(mem.getWord(0));
 	}
 	
 }
