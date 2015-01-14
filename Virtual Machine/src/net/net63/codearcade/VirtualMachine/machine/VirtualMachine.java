e052c7ef9b7760cbc70122af7596cb4ec1d4a328
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-14 22:24:59 +0000

    Use thread run instead of swing utlities to try and make it work

--- Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/VirtualMachine.java
+++ Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/VirtualMachine.java
@@ -1,12 +1,12 @@
 package net.net63.codearcade.VirtualMachine.machine;
 
-import javax.swing.SwingUtilities;
-
 public class VirtualMachine {
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Window());
-
+		Window window = new Window();
+		Thread t = new Thread(window);
+		
+		t.start();
 	}
 
 }
