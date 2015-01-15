f7f17a8b7412d8a0dfb2c9f18eb81969a73c8840
Basim Khajwal <basimkhajwal@gmail.com>
2015-01-15 19:45:34 +0000

    Finished correct launch configurations

--- Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/VirtualMachine.java
+++ Virtual Machine/src/net/net63/codearcade/VirtualMachine/machine/VirtualMachine.java
@@ -1,12 +1,11 @@
 package net.net63.codearcade.VirtualMachine.machine;
 
+
 public class VirtualMachine {
 
 	public static void main(String[] args) {
 		Window window = new Window();
-		Thread t = new Thread(window);
-		
-		t.start();
+		new Thread(window).start();
 	}
 
 }
